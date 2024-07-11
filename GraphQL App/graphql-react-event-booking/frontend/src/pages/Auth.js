b1fd0256cccd9a365ba68dea0110756f64a120ae
Kamva <mtikasoga96@gmail.com>
2024-07-11 08:32:27 +0200

    tutorial 2 graphql frontend and backend connection 2

--- GraphQL App/graphql-react-event-booking/frontend/src/pages/Auth.js
+++ GraphQL App/graphql-react-event-booking/frontend/src/pages/Auth.js
@@ -35,38 +35,47 @@ class AuthPage extends Component {
                 'Content-Type': 'application/json'
             }
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
-    }
 
-    render() {
-        return (
-            <form className='auth-form' onSubmit={this.submitHandler}>
-                <div className='form-control'>
-                    <label htmlFor='email'>Email</label>
-                    <input type='email' id='email' ref={this.emailEl}></input>
-                </div>
-                <div className='form-control'>
-                    <label htmlFor='password'>Password</label>
-                    <input type='password' id='password' ref={this.passwordEl}></input>
-                </div>
-                <div className='form-actions'>
-                    <button type='submit'>Submit</button>
-                    <button type='button'>Switch to Sign Up</button>
-                </div>
-            </form>
-        );
+        render(); {
+            return (
+                <form className='auth-form' onSubmit={this.submitHandler}>
+                    <div className='form-control'>
+                        <label htmlFor='email'>Email</label>
+                        <input type='email' id='email' ref={this.emailEl}></input>
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor='password'>Password</label>
+                        <input type='password' id='password' ref={this.passwordEl}></input>
+                    </div>
+                    <div className='form-actions'>
+                        <button type='submit'>Submit</button>
+                        <button type='button'>Switch to Sign Up</button>
+                    </div>
+                </form>
+            );
+        }
     }
 }
 
 export default AuthPage;
+
+
+// fetch('http://localhost:8000/graphiql', {
+//     method: 'POST',
+//     body: JSON.stringify(requestBody),
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// })
+// .then(response => {
+//     if (!response.ok) {
+//         throw new Error('Network response was not ok ' + response.statusText);
+//     }
+//     return response.json();
+// })
+// .then(data => {
+//     console.log(data);
+// })
+// .catch(error => {
+//     console.error('There was a problem with the fetch operation:', error);
+// });
