1cf3975a6a3782a855d4100aa2ab8cdf655a3cca
Kamva <mtikasoga96@gmail.com>
2024-07-13 22:49:17 +0200

    tutorial 2 graphql refactor token implementation code

--- GraphQL App/graphql-react-event-booking/frontend/src/App.js
+++ GraphQL App/graphql-react-event-booking/frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
@@ -6,37 +7,44 @@ import EventsPage from './pages/Events';
 import MainNavigation from './components/Navigation/MainNavigation';
 import AuthContext from './context/auth-context';
 
-function App() {
+class App extends Component {
   state = {
     tokenId: null,
     userId: null,
   }
 
   login = (token, userId, tokeExpiration) => {
-    this.setState({token: token, userId: userId});
+    this.setState({ token: token, userId: userId });
   }
 
-  logout = () => {}
+  logout = () => { }
 
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <AuthContext.Provider value={{ token: this.state.token, userId: this.state.userId, login: this.login, logout: this.logout }}>
+            <MainNavigation />
+            <main className='main-content'>
+              <Routes>
+                {!this.state.token &&
+                  <Route path="/" element={<Navigate to="/auth" replace />} />}
+                {this.state.token &&
+                  <Route path="/" element={<Navigate to="/events" replace />} />}
+                {this.state.token &&
+                  <Route path="/auth" element={<Navigate to="/events" replace />} />}
+                <Route path='/login' element={null} />
+                {!this.state.token && <Route path='/auth' element={<AuthPage />} />}
+                <Route path='/events' element={<EventsPage />} />
+                {this.state.token && <Route path='/bookings' element={<BookingsPage />} />}
+              </Routes>
+            </main>
+          </AuthContext.Provider>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 
-  return (
-    <BrowserRouter>
-      <React.Fragment>
-        <AuthContext.Provider value={{token: this.state.token, userId: this.state.userId, login: this.login, logout: this.logout}}>
-          <MainNavigation />
-          <main className='main-content'>
-            <Routes>
-              <Route path="/" element={<Navigate to="/auth" replace />} />
-              <Route path='/login' element={null} />
-              <Route path='/auth' element={<AuthPage />} />
-              <Route path='/events' element={<EventsPage />} />
-              <Route path='/bookings' element={<BookingsPage />} />
-            </Routes>
-          </main>
-        </AuthContext.Provider>
-      </React.Fragment>
-    </BrowserRouter>
-  );
 }
 
 export default App;
